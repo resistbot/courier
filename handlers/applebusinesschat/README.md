60cea99b42b5c4aeba4eab41ab9405395c3ecf12
BC Broussard <bc.broussard@flyingcranes.com>
2018-10-30 11:41:44 -0700

    text imessage chat send/recieve

--- handlers/applebusinesschat/README.md
+++ handlers/applebusinesschat/README.md
@@ -1,9 +1,20 @@
 
+## Rapidpro config
+
 ```ini
-location ~ /handlers/facebook/ {
-    rewrite /handlers/facebook/(.*) /c/fb/$1/receive;
+# NGINX handler rewrite for old systems
+location ~ /handlers/applebusinesschat/ {
+    rewrite /handlers/applebusinesschat/(.*) /c/ac/$1/receive;
     proxy_set_header Host $http_host;
     proxy_pass http://courier_server;
     break;
   }
+```
+
+- Create a new Channel `AC` in RapidPro for the config settings
+```bash
+business_id # apple business id
+csp_id # Apple CSP ID
+secret # Apple Secret Key
+
 ```
--- handlers/applebusinesschat/applebusinesschat.go
+++ handlers/applebusinesschat/applebusinesschat.go
@@ -5,21 +5,35 @@ GET /handlers/applebusinesschat/received/uuid?account=12345&dest=8500&message=Ms
 */
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
 	"fmt"
 	"net/http"
-	"net/url"
 	"strings"
+	"sync"
 	"time"
 
 	"github.com/dgrijalva/jwt-go"
 	"github.com/nyaruka/courier"
 	"github.com/nyaruka/courier/handlers"
 	"github.com/nyaruka/courier/utils"
+	"github.com/nyaruka/gocommon/urns"
 	"github.com/pkg/errors"
 )
 
-var ()
+const (
+	configCSPID      = "csp_id"
+	configBusinessID = "business_id"
+	mspAgentName     = "rp-bot"
+)
+
+var (
+	sendURL          = "https://mspgw.push.apple.com/v1/message"
+	decodePayloadURL = "https://mspgw.push.apple.com/v1/decodePayload"
+	preDownloadURL   = "https://mspgw.push.apple.com/v1/preDownload"
+	preUploadURL     = "https://mspgw.push.apple.com/v1/preUpload"
+)
 
 func init() {
 	courier.RegisterHandler(newHandler())
@@ -45,14 +59,26 @@ const (
 	messageTypeInteractive = "interactive"  //Indicates that this is an interactive message.
 	messageTypeTypingStart = "typing_start" //Indicates that the customer is actively entering text in Messages.
 	messageTypeTypingEnd   = "typing_end"   //Indicates that the customer cleared the text they were entering.
-	messageTypeClose       = "close"        //Indicates that the customer closed the chat. Only a customer can close a chat. The CSP should never send a message with this type to Business Chat
+	messageTypeClose       = "close"        //Receive Only - Indicates that the customer closed the chat. Only a customer can close a chat. The CSP should never send a message with this type to Business Chat
+	messageRichLink        = "richLink"     //Send Only - Indicates the message contains rich link data.
 )
 
-// userMessage is the top level message object for all received Apple Business Chat messages
-type userMessage struct {
+// Header Keys
+const (
+	headerAuthorization = "Authorization"  //Credentials used to authenticate the request. See Authorize a Message.
+	headerID            = "id"             //A UUID string that identifies the message.
+	headerSourceID      = "source-id"      //A string that identifies the message sender. When a customer sends a message, the source-id is the customer’s opaque ID. When a business sends a reply to the customer, the source-id is the business ID.
+	headerDestinationID = "destination-id" //A string that identifies the message recipient. When a customer sends a message, the destination-id is the business ID. When a business sends a reply to the customer, the value is the customer’s opaque ID.
+	headerMSPAgent      = "msp-agent"      //A string that identifies the agent or system sending the request. The CSP should set the field to a string value that is meaningful to them. Business Chat logs up to the first 64 characters of the string.
+	headerDeviceAgent   = "device-agent"   //A string that identifies the operating system type of the customer’s device. Possible values include: "iPhone OS", "Mac OS X", "Watch OS", and "Apple TVOS". Business Chat includes this header key in each request it sends to the CSP.
+	headerAutoReply     = "auto-reply"     //A Boolean that determines whether the reply is sent by a person or an automated bot. Set the value to true when an automated agent (a bot) sends the reply.
+)
+
+// message is the top level message object for all sent/received Apple Business Chat messages
+type message struct {
+	ID                 string             `json:"id"`                 //A UUID string that identifies the message.
 	SourceID           string             `json:"sourceId"`           //The customer’s opaque ID identifying the customer as the message sender.
 	DestinationID      string             `json:"destinationId"`      //The business ID of the message recipient.
-	ID                 string             `json:"id"`                 //A UUID string that identifies the message.
 	Body               string             `json:"body"`               //The message text. This field is required when the type is text. There is no predetermined maximum for this field, but there may be a maximum size for the payload. This field supports the Basic Multilingual Plane of unicode.
 	Type               string             `json:"type"`               //A string that identifies the message type. For the list of possible values, see Message Type Values.
 	V                  int                `json:"v"`                  //The message schema version number. Must be 1, which is the current version of the message JSON.
@@ -108,145 +134,239 @@ type interactiveDataRef struct {
 	Bid             string `json:"bid"`              //Identifies the iMessage extension.
 }
 
-type moForm struct {
-	From    string `name:"from"`
-	Sender  string `name:"sender"`
-	Message string `name:"message"`
-	Date    string `name:"date"`
-	Time    string `name:"time"`
-}
-
 // receiveMessage is our HTTP handler function for incoming messages
 // https://developer.apple.com/documentation/businesschat/receiving_messages
 func (h *handler) receiveMessage(ctx context.Context, channel courier.Channel, w http.ResponseWriter, r *http.Request) ([]courier.Event, error) {
+
+	// make sure we have config keys
+	secretKey := channel.StringConfigForKey(courier.ConfigSecret, "")
+	if secretKey == "" {
+		return nil, handlers.WriteAndLogRequestError(ctx, h, channel, w, r, fmt.Errorf("missing secretKey config for AC channel"))
+	}
+
+	cspID := channel.StringConfigForKey(configCSPID, "")
+	if cspID == "" {
+		return nil, handlers.WriteAndLogRequestError(ctx, h, channel, w, r, fmt.Errorf("missing csp_id config for AC channel"))
+	}
+
+	businessID := channel.StringConfigForKey(configBusinessID, "")
+	if businessID == "" {
+		return nil, handlers.WriteAndLogRequestError(ctx, h, channel, w, r, fmt.Errorf("missing business_id config for AC channel"))
+	}
 
 	// 1. Validate message
 	// Verify that the destination-id field in the HTTP request header matches a valid business ID
 	// Validate the Authorization header field
 	// Check for required HTTP request header fields
 	// Verify that the HTTP request body contains JSON data
+	msgDestinationID := r.Header.Get(headerDestinationID)
+	if msgDestinationID != businessID {
+		return nil, handlers.WriteAndLogRequestError(ctx, h, channel, w, r, fmt.Errorf("business_id does not match destination-id header for AC channel"))
+	}
 
-	// // make sure we have an auth token
-	// authToken := channel.StringConfigForKey(courier.ConfigAuthToken, "")
-	// if authToken == "" {
-	// 	return nil, handlers.WriteAndLogRequestError(ctx, h, channel, w, r, fmt.Errorf("missing auth token for FB channel"))
-	// }
+	// Other headers
+	// NOTE: We can re-enable these if we use them
+	// msgID := r.Header.Get(headerID)
+	// msgSourceID := r.Header.Get(headerSourceID)
+	// msgMSPAgent := r.Header.Get(headerMSPAgent)
+	// msgDeviceAgent := r.Header.Get(headerDeviceAgent)
+	// msgAutoReply := r.Header.Get(headerAutoReply)
 
-	// form := &moForm{}
-	// if err := handlers.DecodeAndValidateForm(form, r); err != nil {
-	// 	return nil, handlers.WriteAndLogRequestError(ctx, h, channel, w, r, err)
-	// }
+	// 2. Verify Authorization
+	// CSP-ID
+	// secretKey
 
-	// // must have one of from or sender set, error if neither
-	// sender := form.Sender
-	// if sender == "" {
-	// 	sender = form.From
-	// }
-	// if sender == "" {
-	// 	return nil, handlers.WriteAndLogRequestError(ctx, h, channel, w, r, errors.New("must have one of 'sender' or 'from'"))
-	// }
+	reqToken := r.Header.Get(headerAuthorization)
+	splitToken := strings.Split(reqToken, " ")
+	if len(splitToken) != 2 || strings.ToLower(splitToken[0]) != "bearer" {
+		return nil, handlers.WriteAndLogRequestError(ctx, h, channel, w, r, fmt.Errorf("Authorization header must be in the format Bearer <token>"))
+	}
+	reqToken = splitToken[1]
+	if err := validateToken(reqToken, cspID, secretKey); err != nil {
+		return nil, handlers.WriteAndLogRequestError(ctx, h, channel, w, r, errors.Wrap(err, "Invalid Token:"))
+	}
 
-	// // if we have a date, parse it
-	// dateString := form.Date
-	// if dateString == "" {
-	// 	dateString = form.Time
-	// }
+	msg := &message{}
+	if err := handlers.DecodeAndValidateForm(msg, r); err != nil {
+		return nil, handlers.WriteAndLogRequestError(ctx, h, channel, w, r, err)
+	}
 
-	// var err error
-	// date := time.Now()
-	// if dateString != "" {
-	// 	date, err = time.Parse(time.RFC3339Nano, dateString)
-	// 	if err != nil {
-	// 		return nil, handlers.WriteAndLogRequestError(ctx, h, channel, w, r, errors.New("invalid date format, must be RFC 3339"))
-	// 	}
-	// }
+	// ignore anything other than text messages
+	if msg.Type != messageTypeText {
+		return nil, handlers.WriteAndLogRequestIgnored(ctx, h, channel, w, r, fmt.Sprintf("ignoring non-text request: %s", msg.Type))
+	}
 
-	// // create our URN
-	// urn, err := handlers.StrictTelForCountry(sender, channel.Country())
-	// if err != nil {
-	// 	return nil, handlers.WriteAndLogRequestError(ctx, h, channel, w, r, err)
-	// }
+	// create our URN
+	urn, err := newAppleBusinessChatURN(msg.SourceID)
+	if err != nil {
+		return nil, handlers.WriteAndLogRequestError(ctx, h, channel, w, r, err)
+	}
 
-	// // build our msg
-	// dbMsg := h.Backend().NewIncomingMsg(channel, urn, form.Message).WithReceivedOn(date)
+	// build our msg
+	dbMsg := h.Backend().NewIncomingMsg(channel, urn, msg.Body).WithExternalID(msg.ID)
 
-	// // and finally write our message
-	// return handlers.WriteMsgsAndResponse(ctx, h, []courier.Msg{dbMsg}, w, r)
-	return nil, nil
+	// and finally write our message
+	return handlers.WriteMsgsAndResponse(ctx, h, []courier.Msg{dbMsg}, w, r)
+}
+
+// TODO: Move this to github.com/nyaruka/gocommon/urns
+func newAppleBusinessChatURN(identifier string) (urns.URN, error) {
+	AppleBusinessChatScheme := "applebusinesschat"
+	return urns.NewURNFromParts(AppleBusinessChatScheme, identifier, "", "")
 }
 
 // SendMsg sends the passed in message, returning any error
 func (h *handler) SendMsg(ctx context.Context, msg courier.Msg) (courier.MsgStatus, error) {
 
-	// urlStr := msg.Channel().StringConfigForKey(courier.ConfigBaseURL, "")
-	// baseURL, err := url.Parse(urlStr)
-	// if err != nil {
-	// 	return nil, fmt.Errorf("invalid base url set for AC channel: %s", err)
-	// }
+	// make sure we have config keys
+	secretKey := msg.Channel().StringConfigForKey(courier.ConfigSecret, "")
+	if secretKey == "" {
+		return nil, fmt.Errorf("missing secretKey config for AC channel")
+	}
 
-	// username := msg.Channel().StringConfigForKey(courier.ConfigUsername, "")
-	// if username == "" {
-	// 	return nil, fmt.Errorf("no username set for YO channel")
-	// }
+	cspID := msg.Channel().StringConfigForKey(configCSPID, "")
+	if cspID == "" {
+		return nil, fmt.Errorf("missing csp_id config for AC channel")
+	}
 
-	// password := msg.Channel().StringConfigForKey(courier.ConfigPassword, "")
-	// if password == "" {
-	// 	return nil, fmt.Errorf("no password set for YO channel")
-	// }
+	businessID := msg.Channel().StringConfigForKey(configBusinessID, "")
+	if businessID == "" {
+		return nil, fmt.Errorf("missing business_id config for AC channel")
+	}
 
-	// status := h.Backend().NewMsgStatusForID(msg.Channel(), msg.ID(), courier.MsgErrored)
+	recipientID := msg.URN().Path()
+	text := handlers.GetTextAndAttachments(msg)
 
-	// for _, part := range handlers.SplitMsg(handlers.GetTextAndAttachments(msg), maxMsgLength) {
-	// 	form := url.Values{
-	// 		"origin":       []string{strings.TrimPrefix(msg.Channel().Address(), "+")},
-	// 		"sms_content":  []string{part},
-	// 		"destinations": []string{strings.TrimPrefix(msg.URN().Path(), "+")},
-	// 		"ybsacctno":    []string{username},
-	// 		"password":     []string{password},
-	// 	}
+	status := h.Backend().NewMsgStatusForID(msg.Channel(), msg.ID(), courier.MsgErrored)
 
-	// 	for _, sendURL := range sendURLs {
-	// 		sendURL, _ := url.Parse(sendURL)
-	// 		sendURL.RawQuery = form.Encode()
+	// get auth token
+	authToken, err := generateAuthToken(cspID, secretKey)
+	if err != nil {
+		return status, err
+	}
 
-	// 		req, _ := http.NewRequest(http.MethodGet, sendURL.String(), nil)
-	// 		req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	// build message payload
+	payload := message{V: 1}
+	payload.Type = messageTypeText
+	payload.DestinationID = recipientID
+	payload.SourceID = businessID
+	payload.Body = text
+	payload.ID = msg.UUID().String()
 
-	// 		rr, err := utils.MakeHTTPRequest(req)
-	// 		log := courier.NewChannelLogFromRR("Message Sent", msg.Channel(), msg.ID(), rr).WithError("Message Send Error", err)
-	// 		status.AddLog(log)
+	jsonBody, err := json.Marshal(payload)
+	if err != nil {
+		return status, err
+	}
 
-	// 		if err != nil {
-	// 			continue
-	// 		}
+	req, err := http.NewRequest(http.MethodPost, sendURL, bytes.NewReader(jsonBody))
+	if err != nil {
+		return status, err
+	}
 
-	// 		responseQS, _ := url.ParseQuery(string(rr.Body))
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Accept", "application/json")
 
-	// 		// check whether we were blacklisted
-	// 		createMessage, _ := responseQS["ybs_autocreate_message"]
-	// 		if len(createMessage) > 0 && strings.Contains(createMessage[0], "BLACKLISTED") {
-	// 			status.SetStatus(courier.MsgFailed)
+	req.Header.Set(headerAuthorization, fmt.Sprintf("Bearer %s", authToken))
 
-	// 			// create a stop channel event
-	// 			channelEvent := h.Backend().NewChannelEvent(msg.Channel(), courier.StopContact, msg.URN())
-	// 			err = h.Backend().WriteChannelEvent(ctx, channelEvent)
-	// 			if err != nil {
-	// 				return nil, err
-	// 			}
+	// Set Apple headers
+	// req.Header.Set(headerDestinationID, recipientID)
+	// req.Header.Set(headerSourceID, businessID)
+	req.Header.Set(headerMSPAgent, mspAgentName)
 
-	// 			return status, nil
-	// 		}
+	// TODO: change auto-reply to false if human responding instead of bot
+	req.Header.Set(headerAutoReply, "true")
 
-	// 		// finally check that we were sent
-	// 		createStatus, _ := responseQS["ybs_autocreate_status"]
-	// 		if len(createStatus) > 0 && createStatus[0] == "OK" {
-	// 			status.SetStatus(courier.MsgWired)
-	// 			return status, nil
-	// 		}
-	// 	}
-	// }
+	rr, err := utils.MakeHTTPRequest(req)
+	log := courier.NewChannelLogFromRR("Message Sent", msg.Channel(), msg.ID(), rr).WithError("Message Send Error", err)
+	status.AddLog(log)
 
-	// return status, err
+	if err != nil {
+		return status, nil
+	}
 
-	return nil, nil
+	// this was wired successfully
+	status.SetStatus(courier.MsgWired)
+
+	return status, nil
+}
+
+var (
+	cachedAuthTokenTime time.Time
+	cachedAuthToken     string
+	muAuthToken         *sync.RWMutex //goroutine safe token access
+)
+
+// generateAuthToken creates or re-uses a cached valid JWT with valid Apple Business Chat claims
+//
+// Valid Claims:
+//"alg" - A string identifying the algorithm used to encode the payload.
+//"aud" - A string, or array of strings, identifying the recipients of the JWT. The value should be a string when the JWT has one recipient; otherwise, it should be an array of strings where each string represents a recipient. The aud value is always the CSP ID when exchanging messages with Business Chat.
+//"iss" - A string identifying the principal that issued the JWT. The value is always the CSP ID when exchanging messages with Business Chat.
+//"iat" - A numeric date—that is, an integer—identifying the time at which the JWT was issued. The value is the number of seconds from 1970-01-01T00:00:00Z UTC until the specified UTC date and time, ignoring leap seconds. For more information, see the Terminology section in RFC 7519.
+func generateAuthToken(cspid string, secretKey string) (string, error) {
+	// Token older than 1 hour? expire
+	oneHourAgo := time.Now().Add(-time.Minute * 55)
+
+	// cached token still valid?
+	muAuthToken.RLock()
+	if cachedAuthTokenTime.After(oneHourAgo) {
+		token := cachedAuthToken
+		muAuthToken.RUnlock()
+		return token, nil
+	}
+	muAuthToken.RUnlock()
+
+	// regenerate auth token
+	muAuthToken.Lock()
+	defer muAuthToken.Unlock()
+	// check that another goroutine didnt already generate time/token
+	if cachedAuthTokenTime.Before(oneHourAgo) {
+		now := time.Now()
+		tok := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+			"iat": now.Unix(),
+			"iss": cspid,
+		})
+
+		tokenString, err := tok.SignedString(secretKey)
+		if err != nil {
+			return "", err
+		}
+
+		cachedAuthToken = tokenString
+		cachedAuthTokenTime = now
+	}
+
+	token := cachedAuthToken
+	return token, nil
+}
+
+// validateToken verifies a JWT
+func validateToken(tokenString string, cspid string, secretKey string) error {
+
+	// Parse takes the token string and a function for looking up the key. The latter is especially
+	// useful if you use multiple keys for your application.  The standard is to use 'kid' in the
+	// head of the token to identify which key to use, but the parsed token (head and claims) is provided
+	// to the callback, providing flexibility.
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+		}
+		return []byte(secretKey), nil
+	})
+
+	if claims, ok := token.Claims.(jwt.MapClaims); ok {
+
+		if !token.Valid {
+			return errors.New("Auth token invalid")
+		}
+
+		if !claims.VerifyAudience(cspid, true) {
+			return errors.New("Invalid auth token aud")
+		}
+
+	} else {
+		return errors.New("Could not parse JWT claims")
+	}
+
+	return nil
 }
